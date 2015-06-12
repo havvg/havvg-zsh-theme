6340be62c9a953f70499d3972b14909a001aa850
Toni Uebernickel <tuebernickel@gmail.com>
2015-06-12 19:28:44 +0200

    enable oh-my-git on right prompt

--- havvg.zsh-theme
+++ havvg.zsh-theme
@@ -2,7 +2,5 @@ source $(dirname $0)/oh-my-git.sh
 
 autoload -U colors && colors
 
-if [ $UID -eq 0 ]; then NCOLOR="red"; else NCOLOR="015"; fi
-
 PROMPT='$(build_prompt)$(symfony_env_prompt)$(trndsphere_env_prompt)%{$reset_color%} '
-RPROMPT='$FG[237]%~%{$reset_color%}'
+RPROMPT='$(build_prompt "custom_build_rprompt")'
--- oh-my-git.sh
+++ oh-my-git.sh
@@ -1,7 +1,5 @@
-: ${omg_ungit_prompt:=$PS1}
-: ${omg_second_line:=''}
 
-: ${omg_is_a_git_repo_symbol:=''} # 
+: ${omg_is_a_git_repo_symbol:=''} # 
 
 : ${omg_has_untracked_files_symbol:=''}        #                ?    
 : ${omg_has_adds_symbol:=''}
@@ -17,7 +15,7 @@
 : ${omg_detached_symbol:=''}
 
 : ${omg_has_diverged_symbol:=''}               #   
-: ${omg_can_fast_forward_symbol:=''}
+: ${omg_can_fast_forward_symbol:=''} #   
 : ${omg_should_push_symbol:=' '}                #    
 
 : ${omg_not_tracked_branch_symbol:=''}
@@ -27,25 +25,44 @@
 : ${omg_has_stashes_symbol:=''}
 : ${omg_has_action_in_progress_symbol:=''}     #                  
 
+: ${prompt_eol_symbol:=""}
+: ${prompt_separator:=""}
+
+: ${rprompt_fol_symbol:=""}
+: ${rprompt_separator:=""}
+
 
 function enrich_append {
     local symbol=${1}
     local color=${2:-$FG[015]}
 
-    echo -n " ${color}${symbol}${reset_color}"
+    echo -n " ${color}${symbol}%{$reset_color%}"
 }
 
 function enrich_append_if {
-    if [[ ${1} == true ]]; then
-        enrich_append("${2}", "${3}")
+    if [[ true == ${1} ]]; then
+        enrich_append "${2}" "${3}"
     fi
+}
+
+function enrich_append_unless {
+    if [[ false == ${1} ]]; then
+        enrich_append "${2}" "${3}"
+    fi
+}
+
+function enrich_prepend {
+    local symbol=${1}
+    local color=${2:-$FG[015]}
+
+    echo -n "${color}${symbol} "
 }
 
 function custom_build_prompt {
     local enabled=${1}
     local current_commit_hash=${2}
     local is_a_git_repo=${3}
-    local current_branch=$4
+    local current_branch=${4}
     local detached=${5}
     local just_init=${6}
     local has_upstream=${7}
@@ -76,10 +93,6 @@ function custom_build_prompt {
     local prompt="${white}"
 
     if [[ true == ${is_a_git_repo} ]]; then
-        if [ -n "${omg_is_a_git_repo_symbol}" ]; then
-            prompt+=$(enrich_append ${omg_is_a_git_repo_symbol} "${white}")
-        fi
-
         if [[ true == ${is_on_a_tag} ]]; then
             prompt+=$(enrich_append ${omg_is_on_a_tag_symbol} "${blue}")
             prompt+=$(enrich_append ${tag_at_current_commit} "${blue}")
@@ -92,23 +105,77 @@ function custom_build_prompt {
                     prompt+=$(enrich_append ${omg_not_tracked_branch_symbol} "${blue}")
                 else
                     prompt+=$(enrich_append "${current_branch}" "${blue}")
-                    prompt+=$(enrich_append_if !${will_rebase} "${omg_merge_tracking_branch_symbol}" "${red}")
-
-                    if [[ true == ${has_diverged} ]]; then
-                        prompt+=$(enrich_append ${omg_has_diverged_symbol} "${yellow}")
-                    else
-                        if [[ ${commits_behind} -gt 0 ]]; then
-                            prompt+=$(enrich_append ${omg_can_fast_forward_symbol} "${green}")
-                        fi
-
-                        if [[ ${commits_ahead} -gt 0 ]]; then
-                            prompt+=$(enrich_append ${omg_should_push_symbol} "${white}")
-                        fi
-                    fi
+                    prompt+=$(enrich_append_unless ${will_rebase} "${omg_merge_tracking_branch_symbol}" "${red}")
+                    prompt+=$(enrich_append_if ${has_diverged} ${omg_has_diverged_symbol} "${yellow}")
                 fi
             fi
         fi
     fi
 
     echo "${prompt}"
+}
+
+function custom_build_rprompt() {
+    local enabled=${1}
+    local current_commit_hash=${2}
+    local is_a_git_repo=${3}
+    local current_branch=${4}
+    local detached=${5}
+    local just_init=${6}
+    local has_upstream=${7}
+    local has_modifications=${8}
+    local has_modifications_cached=${9}
+    local has_adds=${10}
+    local has_deletions=${11}
+    local has_deletions_cached=${12}
+    local has_untracked_files=${13}
+    local ready_to_commit=${14}
+    local tag_at_current_commit=${15}
+    local is_on_a_tag=${16}
+    local has_upstream=${17}
+    local commits_ahead=${18}
+    local commits_behind=${19}
+    local has_diverged=${20}
+    local should_push=${21}
+    local will_rebase=${22}
+    local has_stashes=${23}
+    local action=${24}
+
+    local blue="%{$fg[blue]%}"
+    local grey="%{$FG[237]%}"
+    local green="%{$fg[green]%}"
+    local red="%{$fg[red]%}"
+    local white="%{$FG[015]%}"
+    local yellow="%{$fg[yellow]%}"
+
+    local background_color="%{$BG[039]%}"
+
+    local fol_symbol="%{$FG[039]%}${rprompt_fol_symbol}"
+    local separator="${grey}${rprompt_separator}"
+
+    local rprompt=""
+
+    if [[ true == ${is_a_git_repo} ]]; then
+        if [[ true == ${has_upstream} ]]; then
+            if [[ true == ${has_diverged} ]]; then
+                if [[ ${commits_behind} -gt 0 ]]; then
+                    rprompt+=$(enrich_prepend ${omg_can_fast_forward_symbol} "${white}")
+                fi
+            fi
+
+            if [[ ${commits_ahead} -gt 0 && false == ${has_diverged} ]]; then
+                rprompt+=$(enrich_prepend "${omg_should_push_symbol}" "${white}")
+            fi
+        fi
+    fi
+
+    rprompt+="${grey}%2~"
+
+    if [[ true == ${is_a_git_repo} ]]; then
+        if [ -n "${omg_is_a_git_repo_symbol}" ]; then
+            rprompt+=$(enrich_prepend " ${omg_is_a_git_repo_symbol}" "${white}")
+        fi
+    fi
+
+    echo "${fol_symbol}${background_color} ${rprompt}%E%{$reset_color%}"
 }
